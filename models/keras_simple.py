f41614810f4bd5eb337a106bf10ce6c46e2da535
eqtstv <38236287+eqtstv@users.noreply.github.com>
2020-10-29 17:30:33 +0100

    Add model

---
+++ models/keras_simple.py
@@ -0,0 +1,64 @@
+# TensorFlow and tf.keras
+import tensorflow as tf
+from tensorflow import keras
+import sys
+
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+sys.path.insert(0, "..")
+from tfutils import LossAndAccToCsvCallback
+
+fashion_mnist = tf.keras.datasets.fashion_mnist
+
+(train_images, train_labels), (test_images, test_labels) = fashion_mnist.load_data()
+
+train_images = train_images / 255.0
+
+test_images = test_images / 255.0
+
+
+def get_model():
+    model = tf.keras.Sequential(
+        [
+            tf.keras.layers.Flatten(input_shape=(28, 28)),
+            tf.keras.layers.Dense(128, activation="relu"),
+            tf.keras.layers.Dense(10),
+        ]
+    )
+    model.compile(
+        optimizer="adam",
+        loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+        metrics=["accuracy"],
+    )
+    return model
+
+
+model = get_model()
+
+model.fit(
+    train_images,
+    train_labels,
+    epochs=10,
+    callbacks=[LossAndAccToCsvCallback()],
+    validation_split=0.2,
+)
+"""
+model.fit(
+    x_train,
+    y_train,
+    batch_size=128,
+    epochs=2,
+    verbose=0,
+    callbacks=[LossAndErrorPrintingCallback()],
+)
+
+res = model.evaluate(
+    x_test,
+    y_test,
+    batch_size=128,
+    verbose=0,
+    callbacks=[LossAndErrorPrintingCallback()],
+)
+"""
