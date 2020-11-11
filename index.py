55f320f071530647fae46b325fca341206789dd9
eqtstv <38236287+eqtstv@users.noreply.github.com>
2020-11-11 20:15:36 +0100

    Change api imports place

--- index.py
+++ index.py
@@ -1,20 +1,10 @@
 import sys
 
 import ml_visualizer.callbacks
-from ml_visualizer.app import api, app, config
+from ml_visualizer.app import app, config
 from ml_visualizer.layout import app_layout
-from ml_visualizer.resources.data import ClearData
-from ml_visualizer.resources.logs import TrainingLog, ValidationLog
-from ml_visualizer.resources.model import ModelLayers, ModelParams, ModelSummary
 
 app.layout = app_layout
-
-api.add_resource(ClearData, "/clear")
-api.add_resource(ModelParams, "/params")
-api.add_resource(ModelSummary, "/summary")
-api.add_resource(ModelLayers, "/layers")
-api.add_resource(TrainingLog, "/train")
-api.add_resource(ValidationLog, "/val")
 
 
 if __name__ == "__main__":
--- ml_visualizer/app.py
+++ ml_visualizer/app.py
@@ -7,12 +7,21 @@ from flask import Flask
 from flask_restful import Api
 
 from ml_visualizer.database.database import init_db
+from ml_visualizer.resources.data import ClearData
+from ml_visualizer.resources.logs import TrainingLog, ValidationLog
+from ml_visualizer.resources.model import ModelLayers, ModelParams, ModelSummary
 
+CONFIG_PATH = pathlib.Path(__file__).parent.joinpath("config.json").resolve()
 server = Flask("my_app")
 api = Api(server)
 init_db()
 
-CONFIG_PATH = pathlib.Path(__file__).parent.joinpath("config.json").resolve()
+api.add_resource(ClearData, "/clear")
+api.add_resource(ModelParams, "/params")
+api.add_resource(ModelSummary, "/summary")
+api.add_resource(ModelLayers, "/layers")
+api.add_resource(TrainingLog, "/train")
+api.add_resource(ValidationLog, "/val")
 
 with open(CONFIG_PATH) as f:
     config = json.load(f)
