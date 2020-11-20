7e2000b0630e7d0dba95a4ad31955483ddfec5dd
eqtstv <38236287+eqtstv@users.noreply.github.com>
2020-11-20 20:28:13 +0100

    Add db tests

--- test/test_log_database.py
+++ test/test_log_database.py
@@ -3,7 +3,7 @@ import unittest
 import sqlalchemy
 from sqlalchemy import inspect, Column, Float, Integer
 from ml_visualizer.database.database import Base, db_session, engine, init_db
-from ml_visualizer.database.models import LogTraining, LogValidation
+from ml_visualizer.database.models import LogTraining, LogValidation, User, Projects
 
 
 class TestDatabase(unittest.TestCase):
@@ -37,6 +37,18 @@ class TestLogTraining(unittest.TestCase):
             self.assertEqual(columns[i][0], valid_column_names[i])
             self.assertIsInstance(columns[i][1], valid_column_types[i])
 
+    def test_log_training_init(self):
+        step = 1
+        batch = 10
+        train_acc = 0.7
+        train_loss = 0.5
+
+        init_row = LogTraining(step, batch, train_acc, train_loss)
+        self.assertEqual(init_row.step, step)
+        self.assertEqual(init_row.batch, batch)
+        self.assertEqual(init_row.train_accuracy, train_acc)
+        self.assertEqual(init_row.train_loss, train_loss)
+
 
 class TestLogValidation(unittest.TestCase):
     def test_log_validaton_table_name(self):
@@ -61,3 +73,29 @@ class TestLogValidation(unittest.TestCase):
         for i, k in enumerate(columns):
             self.assertEqual(columns[i][0], valid_column_names[i])
             self.assertIsInstance(columns[i][1], valid_column_types[i])
+
+    def test_log_validation_init(self):
+        step = 10
+        val_acc = 0.6
+        val_loss = 0.7
+        epoch = 3
+        epoch_time = 4.2
+
+        init_row = LogValidation(step, val_acc, val_loss, epoch, epoch_time)
+        self.assertEqual(init_row.step, step)
+        self.assertEqual(init_row.val_accuracy, val_acc)
+        self.assertEqual(init_row.val_loss, val_loss)
+        self.assertEqual(init_row.epoch, epoch)
+        self.assertEqual(init_row.epoch_time, epoch_time)
+
+
+class TestProjects(unittest.TestCase):
+    def test_init_projects(self):
+        user_id = 1
+        project_name = "myproject"
+        project_description = "my project description"
+
+        init_row = Projects(user_id, project_name, project_description)
+        self.assertEqual(init_row.user_id, user_id)
+        self.assertEqual(init_row.project_name, project_name)
+        self.assertEqual(init_row.project_description, project_description)
