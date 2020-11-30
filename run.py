a16ce00b3301d7412f50ae8ff4720b43ece0a17c
eqtstv <38236287+eqtstv@users.noreply.github.com>
2020-11-30 18:32:37 +0100

    Add switch between running in debug / production mode

--- run.py
+++ run.py
@@ -1,6 +1,17 @@
+import os
+import sys
+
+import waitress
+
 import ml_visualizer.dash_app.callbacks
 from ml_visualizer.app import config, server
 from ml_visualizer.dash_app.dash_app import dash_app
 
 if __name__ == "__main__":
-    server.run(host=f"{config['ip']}", port=f"{config['port']}", debug=True)
+    if sys.argv[1] == "debug":
+        server.run(host=config["ip"], port=config["port"], debug=True)
+
+    if sys.argv[1] == "production":
+        server.debug = False
+        port = int(os.environ.get("PORT", config["port"]))
+        waitress.serve(server, port=port)
--- run_tests.bat
+++ run_tests.bat
@@ -1,3 +1,3 @@
-start python run.py
+start python run.py debug
 timeout 5
 python36 -m unittest discover
